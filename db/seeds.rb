14e16b18e0cf0f30148e1e6f147ff4bf5849d533
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-20 10:27:15 -0300

    seed creates locations with image urls. no lat, lon as not required

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+locations = [
+  ['Paris', 'France', 'Europe'],
+  ['New York', 'USA', 'North America'],
+  ['London', 'England', 'Europe'],
+  ['Bangkok', 'Thailand', 'Asia'],
+  ['Hong Kong', 'China', 'Asia'],
+  ['Dubai', 'UAE', 'Middle East'],
+  ['Singapore', 'Singapore', 'Asia'],
+  ['Rome', 'Italy', 'Europe'],
+  ['Macau', 'Portugal', 'Europe'],
+  ['Istanbul', 'Turkey', 'Europe'],
+  ['Kuala Lumpur', 'Malaysia', 'Asia'],
+  ['Delhi', 'India', 'Asia'],
+  ['Tokyo', 'Japan', 'Asia'],
+  ['Antalya', 'Turkey', 'Asia'],
+  ['Mexico City', 'Mexico', 'North America'],
+  ['Moscow', 'Russia', 'Asia'],
+  ['Porto', 'Portugal', 'Europe'],
+  ['San Francisco', 'USA', 'North America'],
+  ['Beijing', 'China', 'Asia'],
+  ['Los Angelos', 'USA', 'North America'],
+  ['Chicago', 'USA', 'North America'],
+  ['Barcelona', 'Spain', 'Europe'],
+  ['Abu Dhabi', 'UAE', 'Middle East'],
+  ['Amsterdam', 'Netherlands', 'Europe'],
+  ['Madrid', 'Spain', 'Europe'],
+  ['Sydney', 'Australia', 'Asia'],
+  ['San Miguel de Allende', 'Mexico', 'North America'],
+  ['Lisbon', 'Portugal', 'Europe'],
+  ['Vienna', 'Austria', 'Germany'],
+  ['Johannesburg', 'South Africa', 'Africa']
+]
+
+
+locations.each do |location|
+  l = Location.create(city: location[0], country: location[1], region: location[2])
+  photos = Unsplash::Photo.search(location[0])
+                          .first(5)
+                          .map { |result| result.urls["raw"] }
+  photos.each do |photo|
+    i = Image.create(url: photo, location: l)
+  end
+
+  puts l
+end
