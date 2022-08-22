34dad04940edd533783f74699fdbd1e0bbee9fd1
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-22 09:27:15 -0300

    added stimulus controller to search form and collapsing function

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SearchFormController from "./search_form_controller"
+application.register("search-form", SearchFormController)
