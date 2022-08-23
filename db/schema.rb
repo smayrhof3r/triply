b08eae85cd4e3820324b93f0c681622f3a41be0b
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-08-23 20:12:51 +0100

    addhome_city external reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_20_072353) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_185748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "airports", force: :cascade do |t|
+    t.string "name"
+    t.bigint "location_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_airports_on_location_id"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "passenger_group_id", null: false
@@ -25,18 +61,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_072353) do
   end
 
   create_table "flights", force: :cascade do |t|
-    t.bigint "departure_city_id"
-    t.bigint "arrival_city_id"
     t.datetime "departure_time"
     t.datetime "arrival_time"
-    t.string "departure_airport"
-    t.string "arrival_airport"
     t.float "cost_per_head"
     t.string "flight_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["arrival_city_id"], name: "index_flights_on_arrival_city_id"
-    t.index ["departure_city_id"], name: "index_flights_on_departure_city_id"
+    t.bigint "departure_airport_id"
+    t.bigint "arrival_airport_id"
+    t.index ["arrival_airport_id"], name: "index_flights_on_arrival_airport_id"
+    t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.bigint "location_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_images_on_location_id"
   end
 
   create_table "itineraries", force: :cascade do |t|
@@ -89,18 +131,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_20_072353) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "home_city"
+    t.bigint "home_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["home_city_id"], name: "index_users_on_home_city_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "airports", "locations"
   add_foreign_key "bookings", "flights"
   add_foreign_key "bookings", "passenger_groups"
-  add_foreign_key "flights", "locations", column: "arrival_city_id"
-  add_foreign_key "flights", "locations", column: "departure_city_id"
+  add_foreign_key "flights", "locations", column: "arrival_airport_id"
+  add_foreign_key "flights", "locations", column: "departure_airport_id"
+  add_foreign_key "images", "locations"
   add_foreign_key "itineraries", "locations", column: "destination_id"
   add_foreign_key "passenger_groups", "itineraries"
   add_foreign_key "passenger_groups", "locations", column: "origin_city_id"
   add_foreign_key "permissions", "itineraries"
   add_foreign_key "permissions", "users"
+  add_foreign_key "users", "locations", column: "home_city_id"
 end
