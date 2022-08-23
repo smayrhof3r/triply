75bfa128e3b42d4d0997b2f2231b5a4ed6168dbf
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-08-23 20:53:38 +0100

    addtabletousers

--- db/migrate/20220823183542_delete_home_city_from_users.rb
+++ db/migrate/20220823183542_delete_home_city_from_users.rb
@@ -1,4 +1,5 @@
 class DeleteHomeCityFromUsers < ActiveRecord::Migration[7.0]
+
   def change
     remove_column :users, :home_city, :string
   end
--- db/migrate/20220823185748_add_home_city_to_reference_users.rb
+++ db/migrate/20220823185748_add_home_city_to_reference_users.rb
@@ -1,4 +1,5 @@
 class AddHomeCityToReferenceUsers < ActiveRecord::Migration[7.0]
+
   def change
     change_table :users do |t|
       t.references :home_city, index: true, foreign_key: { to_table: :locations }
