7c9b557befc764574f940b1b8a665eeb3c1b56a4
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-08-24 22:51:19 +0100

    add city code to locations for amadeus search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_212536) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_215008) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -100,6 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_212536) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city_code"
   end
 
   create_table "passenger_groups", force: :cascade do |t|
