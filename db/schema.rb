a187e8b548fece5d35710f16bdfd8609ef9c3c54
Emily Santos <eyssantos@gmail.com>
2022-08-25 18:41:31 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_215008) do
   end
 
   create_table "airports", force: :cascade do |t|
-    t.string "name"
+    t.string "code"
     t.bigint "location_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "code"
+    t.string "name"
     t.index ["location_id"], name: "index_airports_on_location_id"
   end
 
