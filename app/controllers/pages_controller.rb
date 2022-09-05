df06df755c982744f9af9c8e41d1a96968ad90e9
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-09-05 22:19:17 +0100

    popular destinations work in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
     Itinerary.delete_unclaimed
+    @locations = Search::DESTINATIONS.sample(3)
+    # from code i need to make it city
+    # images.location_id = locations.id
+    # for each city i need photo
+    # image.url
+    # @image_popular = @location.image
+    end
   end
-end
