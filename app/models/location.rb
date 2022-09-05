52e76ae0d3d08f47053f1361b256540cc6e51c80
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-09-05 23:36:37 +0100

    added exceptions list as new york wasn't well seeded

--- app/models/location.rb
+++ app/models/location.rb
@@ -15,10 +15,17 @@ class Location < ApplicationRecord
   private
 
   def scrape_lonely_planet
-    html_content = Faraday.get("https://www.lonelyplanet.com/places?q=#{city}").body
-    doc = Nokogiri::HTML(html_content)
+    manual_city_links = {
+      "NYC" => "/usa/new-york-city"
+    }
 
-    page_link = doc.search(".card-link").first.attributes["href"].value
+    if manual_city_links[city_code]
+      page_link = manual_city_links[city_code]
+    else
+      html_content = Faraday.get("https://www.lonelyplanet.com/places?q=#{city}").body
+      doc = Nokogiri::HTML(html_content)
+      page_link = doc.search(".card-link").first.attributes["href"].value
+    end
 
     html_content = Faraday.get("https://www.lonelyplanet.com#{page_link}").body
     doc = Nokogiri::HTML(html_content)
