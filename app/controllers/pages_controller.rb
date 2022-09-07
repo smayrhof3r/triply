178184c197fc003b064f6a993fa9b585c8995180
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-09-07 23:19:45 +0100

    popular destinations on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
     Itinerary.delete_unclaimed
-    @locations = Search::DESTINATIONS.sample(3)
+    @popular_images = []
+    Search::DESTINATIONS.sample(3).each do |location|
+      @popular_images << Location.find_by(city_code: location).images.first
     end
   end
+end
