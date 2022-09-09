3d2425e6ba4611fc3617d384c8c651c3541f64cf
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-09-09 10:10:04 +0100

    added json column to searches for amadeus_response

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_204945) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_090759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,6 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_204945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "returnDate"
+    t.json "amadeus_response", default: {}
   end
 
   create_table "users", force: :cascade do |t|
