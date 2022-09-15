e0b6db7e4894a2003813b943e7f4f96db65ed67f
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-09-15 19:42:17 +0100

    add user functionality

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -3,13 +3,14 @@ class PermissionsController < ApplicationController
   def create
     p = Permission.new(permission_params)
     session[:invited_user] = nil
+
     if p.save && p.role == "owner"
       Itinerary.delete_unclaimed(session[:itineraries])
       redirect_to itinerary_path(params["itinerary_id"]), notice: "Itinerary saved! Add flights below."
     elsif p.save && p.role == "guest"
       redirect_to user_path(current_user), notice: "User #{p.user.email} now has access"
     else
-      redirect_to itinerary_path(params["itinerary_id"]), notice: "Failed to save itinerary! Are you logged in?"
+      redirect_to itinerary_path(params["itinerary_id"]), notice: "Failed to save or share itinerary! Are you logged in?"
     end
   end
 
