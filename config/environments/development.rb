543ea8320d07da739aeedae570389d44ac8cedf8
Emily Santos <eyssantos@gmail.com>
2022-09-17 14:41:38 +0100

    email sharing added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,21 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"], # you can use ordinary gmail username here
+    password: ENV["GMAIL_PASSWORD"]   # you can use your gmail password here, but don't push the changes
+  }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
