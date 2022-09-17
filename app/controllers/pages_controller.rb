d33ae9b0cf172fbc450a2c456a0e49493878cb1e
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-09-17 16:01:30 +0100

    commented out itineray deletion on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
-    Itinerary.delete_unclaimed
+    # Itinerary.delete_unclaimed
     @popular_images = []
     Search::DESTINATIONS.sample(3).each do |location|
       @popular_images << Location.find_by(city_code: location).images.first
