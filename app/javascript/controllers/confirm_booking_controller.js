0af7b43e4396b6b69e249a6136877491dba4713d
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-09-22 21:02:33 +0100

    updated stimulus to reference booking path for right form when clicking button

--- app/javascript/controllers/confirm_booking_controller.js
+++ app/javascript/controllers/confirm_booking_controller.js
@@ -7,20 +7,22 @@ export default class extends Controller {
 
   connect() {
     // console.log(this.bookingTarget.innerHTML);
+    console.log('booking stimulus6');
   }
 
   updateBooking (event) {
     event.preventDefault()
-    const url = this.formTarget.action
-
-    fetch(url, { 
-      method: "PATCH", 
+    const form = document.getElementById(`edit_booking_${event.currentTarget.name}`)
+    const url = form.action
+    console.log(url)
+    fetch(url, {
+      method: "PATCH",
       headers: { "Accept": "text/plain" },
-      body: new FormData(this.formTarget)
+      body: new FormData(form)
     })
     .then(response => response.text() )
     .then((data) => {
       this.bookingTarget.outerHTML = data
-    })     
+    })
   }
 }
